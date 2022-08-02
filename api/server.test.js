852a080ef8c98e4bc1e8e17076038cb08dc4806e
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-08-01 22:36:46 -0400

    added testing

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 // Write your tests here
-//const request = require('supertest');
+const request = require('supertest');
 const db = require('../data/dbConfig');
-//const server = require('./server');
+const server = require('./server');
 
 test('sanity', () => {
   expect(true).toBe(true)
@@ -10,3 +10,43 @@ beforeAll( async ()=> {
   await db.migrate.rollback()
   await db.migrate.latest()
 })
+beforeEach(async ()=> {
+  await db('users').truncate()
+})
+afterAll( async ()=> {
+  await db.destroy()
+})
+
+describe('[POST] /register', () => {
+  test('responds with error when no username is entered', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: '', 
+      password: 'abcd',
+    })
+    expect(res.body).toMatchObject({message: 'username and password required'})
+  })
+  test('responds with error when no password is entered', async () => {
+    const res = await request(server).post('/api/auth/register').send({
+      username: 'bill', 
+      password: '',
+    })
+    expect(res.body).toMatchObject({message: 'username and password required'})
+  })
+})
+describe('[POST] /login', () => {
+  test('responds with error when no username is entered', async () => {
+    const res = await request(server).post('/login').send({
+      username: '', 
+      password: 'whatever'
+    })
+    expect(res.status).toBe(500)
+  })
+  test('responds with error when no password is entered', async () => {
+    const res = await request(server).post('/api/auth/login').send({
+      username: 'harry', 
+      password: '',
+    })
+    expect(res.body).toMatchObject({message: 'username and password required'})
+  })
+}) 
+
